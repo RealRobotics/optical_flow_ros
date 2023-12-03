d433f95aac4112ddccddafa0daeb228f2cc9df80
Andy Blight <a.j.blight@leeds.ac.uk>
2023-12-03 14:30:05 +0000

    Simplified launch file

--- launch/optical_flow_launch.py
+++ launch/optical_flow_launch.py
@@ -1,3 +1,4 @@
+# Copyright 2023 University of Leeds.
 # Copyright (c) 2023 Aditya Kamath
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,46 +14,26 @@
 # limitations under the License.
 
 from launch import LaunchDescription
-from launch.actions import EmitEvent, RegisterEventHandler
-from launch.events import matches_action
 from launch.substitutions import PathJoinSubstitution
-from launch_ros.actions import LifecycleNode
-from launch_ros.event_handlers import OnStateTransition
-from launch_ros.events.lifecycle import ChangeState
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from lifecycle_msgs.msg import Transition
 
 def generate_launch_description():
-    ld = LaunchDescription()
-    
+
     optical_flow_params_path = PathJoinSubstitution(
         [FindPackageShare("optical_flow_ros"), "config", "sensor_params.yaml"])
-           
-    optical_flow_node = LifecycleNode(
+
+    optical_flow_node = Node(
         package='optical_flow_ros',
         executable='optical_flow_node',
         name='optical_flow',
-        namespace='', # Do not change, else config params and remappings need to be updated
+        # Do not change, else config params and remappings need to be updated
+        namespace='',
         output='screen',
         parameters=[optical_flow_params_path],
-        remappings=[('odom', 'flow_odom')])
+        # remappings=[('odom', 'flow_odom')]
+    )
 
-    emit_configure_event = EmitEvent(
-        event=ChangeState(
-            lifecycle_node_matcher = matches_action(optical_flow_node),
-            transition_id = Transition.TRANSITION_CONFIGURE))
-
-    register_activate_handler = RegisterEventHandler(
-        OnStateTransition(
-            target_lifecycle_node=optical_flow_node, goal_state='inactive',
-            entities=[
-                EmitEvent(
-                    event=ChangeState(
-                        lifecycle_node_matcher = matches_action(optical_flow_node),
-                        transition_id = Transition.TRANSITION_ACTIVATE))]))
-
+    ld = LaunchDescription()
     ld.add_action(optical_flow_node)
-    ld.add_action(emit_configure_event)
-    ld.add_action(register_activate_handler)
-
     return ld
